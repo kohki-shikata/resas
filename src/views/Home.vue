fb3075ff6e4fec2a455001aa7ba3f1d67e7a5c27
Kohki SHIKATA <no-replay@example.com>
2019-04-14 06:28:05 +0900

    Constructed Main view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+.main
+  .prefs
+    ol
+      li(v-for="item in this.$store.state.prefs")
+        input(type="checkbox" :data-pref-code="item.prefCode" :ref="item.prefCode" @click="getPrefPopulation(item.prefCode)")
+        | {{ item.prefCode }} - {{ item.prefName }}
+  .chart
+    highcharts(:options="this.$store.state.chartData")
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mapState } from 'vuex'
+import { Chart } from 'highcharts-vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    highcharts: Chart
+  },
+  data () {
+    return {
+      chartOpts  
+    }
+  },
+  computed: mapState([
+    'prefs',
+    'chartData'
+  ]),
+  methods: {
+    getPrefPopulation(id) {
+      this.$store.dispatch('getPopulation',id)
+    }
+  },
+  mounted () {
+    this.$store.dispatch('loadPrefs')
+    this.$store.dispatch('loadYears')
+    // this.$store.state.chartData = JSON.parse(localStorage.getItem('prefData'))
   }
 }
 </script>
+
+<style lang="stylus">
+.prefs
+  display flex
+  flex-flow row wrap
+  li
+    list-style-type none
+    display inline-block
+    width 20%
+</style>
+
