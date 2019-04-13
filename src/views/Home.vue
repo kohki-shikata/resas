889bcdff06344f486316cefef4e1bf017b2e3d24
Kohki SHIKATA <no-replay@example.com>
2019-04-14 07:44:45 +0900

    Dress up checkboxes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,13 @@
 <template lang="pug">
 .main
+  .title
+    h1 日本の人口推移
   .prefs
     ol
       li(v-for="item in this.$store.state.prefs")
-        input(type="checkbox" :data-pref-code="item.prefCode" :ref="item.prefCode" @click="getPrefPopulation(item.prefCode)")
-        | {{ item.prefCode }} - {{ item.prefName }}
+        input(type="checkbox" :id="item.prefName" :data-pref-code="item.prefCode" :ref="item.prefCode" @click="getPrefPopulation(item.prefCode)")
+        label(:for="item.prefName")
+          | {{ item.prefName }}
   .chart
     highcharts(:options="this.$store.state.chartData")
 </template>
@@ -36,12 +39,38 @@ export default {
 </script>
 
 <style lang="stylus">
+h1
+  padding .5em
+  background #bee2f1
+  margin-top 0
+  text-align center
 .prefs
   display flex
   flex-flow row wrap
   li
     list-style-type none
     display inline-block
-    width 20%
+    width 10%
+    margin-bottom 1em
+  input
+    display none
+  label
+    position relative
+    padding-left 1.5em
+    &:before
+      content '■'
+      color #eee
+      position absolute
+      top 0
+      left 0
+  input:checked + label
+    &:after
+      content '✔'
+      color red
+      font-size 70%
+      position absolute
+      top 3px
+      left 3px
+
 </style>
 
