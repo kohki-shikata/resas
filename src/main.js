818c021dee3c24d5faabe2caa2df37ca62929088
Kohki SHIKATA <no-replay@example.com>
2019-04-14 06:27:02 +0900

    init plugins

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import VueLodash from 'vue-lodash'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import HighCharts from 'highcharts-vue'
 
 Vue.config.productionTip = false
+
+Vue.use(VueLodash)
+Vue.use(VueAxios, axios)
+Vue.use(HighCharts)
 
 new Vue({
   router,
