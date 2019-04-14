58b0c255764784f3b541925776f4b9c902c097a7
Kohki SHIKATA <no-replay@example.com>
2019-04-14 13:06:26 +0900

    fix missindent

--- src/components/prefectures.vue
+++ src/components/prefectures.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
 .prefs
-    ol
-      li(v-for="item in this.$store.state.prefs")
-        input(type="checkbox" :id="item.prefName" :data-pref-code="item.prefCode" :ref="item.prefCode" @click="getPrefPopulation(item.prefCode)")
-        label(:for="item.prefName")
-          | {{ item.prefName }}
+  ol
+    li(v-for="item in this.$store.state.prefs")
+      input(type="checkbox" :id="item.prefName" :data-pref-code="item.prefCode" :ref="item.prefCode" @click="getPrefPopulation(item.prefCode)")
+      label(:for="item.prefName")
+        | {{ item.prefName }}
 </template>
 
 <script>
